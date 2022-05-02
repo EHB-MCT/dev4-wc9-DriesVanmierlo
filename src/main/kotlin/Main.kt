281e07acca35de7c43a02b672c560014ecfc8c1e
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-05-02 09:57:43 +0200

    Completed puzzle one, day one

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,4 +6,33 @@ fun main(args: Array<String>) {
     //println(reader.getPuzzle3())
     //println(reader.getPuzzle4Numbers())
     //println(reader.getPuzzle4Cards())
+
+    puzzleOne()
+}
+
+fun puzzleOne(){
+    var answer:Int = 0
+    var currentIndex:Int = 0
+    var previousIndex: Int = 0
+    var currentNumber: Int
+    var previousNumber: Int
+    var list = FileReader().getPuzzle1()
+
+    for (number in list){
+
+        previousIndex = currentIndex - 1
+
+        if (previousIndex > -1){
+            currentNumber = list.elementAt(currentIndex)
+            previousNumber = list.elementAt(previousIndex)
+
+            if (currentNumber > previousNumber){
+                answer++
+            }
+        }
+
+        currentIndex ++
+    }
+
+    println(answer)
 }
---
+++ src/main/kotlin/day-1/PuzzleOne.kt
@@ -0,0 +1,15 @@
+package `day-1`
+
+import FileReader
+import java.io.File
+
+class PuzzleOne {
+
+//    fun calculateIncreases(list: List<Int>): Int{
+//        val url = "../src/main/resources/puzzle1.txt"
+//        var file = File(url).readLines()
+//        //println(file);
+//        val inputList = file.map { it.toInt() }
+//        return inputList;
+//    }
+}
